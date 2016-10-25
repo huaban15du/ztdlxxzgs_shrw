d7f678f251a59cbe8e06068b3c3ce2f7869cac9e
wangmeihong <172211854@qq.com>
2016-10-25 14:33:08 +0800

    V0.1--框架搭建
    1. 搭建了框架；

---
+++ shrwApp/shrwApp/commonDefine.h
@@ -0,0 +1,22 @@
+//
+//  commonDefine.h
+//  tiandituDemo
+//
+//  Created by camelroyu on 16/8/11.
+//  Copyright (c) 2016年 com.map1000. All rights reserved.
+//
+
+
+#pragma mark - Common
+//状态栏高度
+#define STATUS_BAR_HEIGHT 20
+//NavBar高度
+#define NAVIGATION_BAR_HEIGHT 44
+//状态栏 ＋ 导航栏 高度
+#define STATUS_AND_NAVIGATION_HEIGHT ((STATUS_BAR_HEIGHT) + (NAVIGATION_BAR_HEIGHT))
+//屏幕 rect
+//#define STATUS_AND_NAVIGATION_HEIGHT ((STATUS_BAR_HEIGHT) + (NAVIGATION_BAR_HEIGHT))
+#define SCREEN_RECT ([UIScreen mainScreen].bounds)
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+#define CONTENT_HEIGHT (SCREEN_HEIGHT - NAVIGATION_BAR_HEIGHT - STATUS_BAR_HEIGHT)
---
+++ shrwApp/shrwAppTests/shrwAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  shrwAppTests.swift
+//  shrwAppTests
+//
+//  Created by camelroyu on 16/10/25.
+//  Copyright (c) 2016年 com.map1000. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class shrwAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
